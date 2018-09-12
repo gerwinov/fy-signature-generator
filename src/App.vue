79370a79909527717e706eb5a15279e4964b73bd
Gerwin Overeem <gerwin@foryard.nl>
2018-09-12 10:30:41 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="input">
+      <h1>FORYARD EMAIL SIGNATURE GENERATOR</h1>
+      <p>Fill in these fields, copy the contents of the block below and set it as your email signature.</p>
+
+      <label for="name">Full name</label><br>
+      <input type="text" v-model="name" name="name" id="name" placeholder="John Doe">
+
+      <br><br>
+
+      <label for="role">Role</label><br>
+      <input type="text" v-model="role" name="role" id="role" placeholder="Developer">
+
+      <br><br>
+
+      <label for="phonenumber">Phone number</label><br>
+      +316<input type="number" v-model="phoneNumber" name="phonenumber" id="phonenumber" placeholder="12345678">
+    </div>
+
+    <div id="signature">
+      <signature-generator :name="name" :role="role" :phone-number="phoneNumber" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignatureGenerator from './components/SignatureGenerator.vue'
 
 export default {
   name: 'app',
+
   components: {
-    HelloWorld
+    SignatureGenerator
+  },
+
+  data () {
+    return {
+      name: '',
+      role: '',
+      phoneNumber: ''
+    }
   }
 }
 </script>
 
 <style>
-#app {
+#input {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-bottom: 50px;
+}
+
+#input label {
+  font-size: 18px;
+}
+
+#input h1 {
+  color: #57EA8A;
+}
+
+#signature {
+  border: 1px solid #2c3e50;
+  border-radius: 10px;
+  padding: 50px;
 }
 </style>
---
+++ src/components/SignatureGenerator.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <br><br><br>
+
+    <div style="font-family:Arial, Calibri, Helvetica, sans-serif; text-decoration: none; font-size: 9pt; color:#202D3A;">
+      <p>Met vriendelijke groet,</p>
+    </div>
+    <p><span><a href="https://foryard.nl/"><img width="170" style="text-decoration: none; color:#202D3A; width: 100%; max-width: 170px;" src="https://foryard.nl/foryard-email-signature.png" alt="Logo Foryard"/></a></span>
+    </p>
+    <div style="font-family:Arial, Calibri, Helvetica, sans-serif; text-decoration: none; font-size: 9pt; color:#202D3A; margin-left:38px;">
+      <p>
+        <span style="color: #00263a; letter-spacing:1px;"><strong>{{ formattedName }}<br></strong></span><span style="color:#9EAFC4"><em>{{ formattedRole }}</em></span>
+      </p>
+      <p>
+        <span><a style="text-decoration: none; color: #202D3A;" :href="'tel:00316' + phoneNumber">{{ '+31(0)6 ' + formattedPhoneNumber }}<br></a></span>
+      </p>
+    </div>
+
+    <br><br>
+
+    <div style="font-family:Arial, Calibri, Helvetica, sans-serif; text-decoration: none; font-size: 9pt; color:#9EAFC4;">
+      <br>
+      <p><span><a style="text-decoration: none; color: #9EAFC4;" href="https://foryard.nl/">Foryard.nl</a>  ●  <a style="text-decoration: none; color: #9EAFC4;" href="tel:0031880040600">+31 (0)88 004 0600</a>  ●  <a style="text-decoration: none; color: #9EAFC4;" href="https://www.google.nl/maps/place/Kuipersstraat+35,+1074+EE+Amsterdam/@52.3541686,4.9029117,17z/data=!3m1!4b1!4m5!3m4!1s0x47c6098f8c959307:0xdaab052fb0fe66e7!8m2!3d52.3541686!4d4.9051004">Kuipersstraat 35, Amsterdam (NIEUW!)</a></span></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignatureGenerator',
+
+  props: {
+    name: String,
+    role: String,
+    phoneNumber: String
+  },
+
+  computed: {
+    formattedName () {
+      return this.name.toUpperCase()
+    },
+
+    formattedRole () {
+      return this.role.charAt(0).toUpperCase() + this.role.slice(1);
+    },
+
+    formattedPhoneNumber () {
+      let formattedNumber = []
+
+      this.phoneNumber.split('').forEach((element, key) => {
+        if (key % 2 === 0 && key !== 0) {
+          formattedNumber.push(' ')
+        }
+        formattedNumber.push(element)
+      })
+
+      return formattedNumber.join('')
+    }
+  },
+}
+</script>
