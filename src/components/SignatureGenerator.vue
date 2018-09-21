134b3a1e8831c8052cdac7fc4ea1ced710419d0c
Gerwin Overeem <gerwin@foryard.nl>
2018-09-21 16:19:14 +0200

    Changed order of elements

--- src/components/SignatureGenerator.vue
+++ src/components/SignatureGenerator.vue
@@ -5,8 +5,6 @@
     <div style="font-family:Arial, Calibri, Helvetica, sans-serif; text-decoration: none; font-size: 9pt; color:#202D3A;">
       <p>Met vriendelijke groet,</p>
     </div>
-    <p><span><a href="https://foryard.nl/"><img width="170" style="text-decoration: none; color:#202D3A; width: 100%; max-width: 170px;" src="https://foryard.nl/foryard-email-signature.png" alt="Logo Foryard"/></a></span>
-    </p>
     <div style="font-family:Arial, Calibri, Helvetica, sans-serif; text-decoration: none; font-size: 9pt; color:#202D3A; margin-left:38px;">
       <p>
         <span style="color: #00263a; letter-spacing:1px;"><strong>{{ formattedName }}<br></strong></span><span style="color:#9EAFC4"><em>{{ formattedRole }}</em></span>
@@ -15,7 +13,8 @@
         <span><a style="text-decoration: none; color: #202D3A;" :href="'tel:00316' + phoneNumber">{{ '+31(0)6 ' + formattedPhoneNumber }}<br></a></span>
       </p>
     </div>
-
+    <p><span><a href="https://foryard.nl/"><img width="170" style="text-decoration: none; color:#202D3A; width: 100%; max-width: 170px;" src="https://foryard.nl/foryard-email-signature.png" alt="Logo Foryard"/></a></span>
+    </p>
     <br><br>
   </div>
 </template>
